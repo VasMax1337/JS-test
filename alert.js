85f9469cb674556ffc6d60ea4dbd3f1fd31330a8
VasMax1337 <meyra698@yandex.ru>
2025-03-17 14:36:36 +0300

    function

--- alert.js
+++ alert.js
@@ -59,94 +59,131 @@
 
 
 
-let age = prompt("Сколько вам лет?")
+// let age = prompt("Сколько вам лет?")
 
-if(age<18) {
-    alert("Вы не совершеннолетний!") 
-}else {
-    alert("Вы совершеннолетний")
-}
-
-
-let num = 1;
-
-for (num = 1; num <= 10; num++) {
-    alert(num)
-}
+// if(age<18) {
+//     alert("Вы не совершеннолетний!") 
+// }else {
+//     alert("Вы совершеннолетний")
+// }
 
 
-let day = prompt("Какое сегодня число?")
+// let num = 1;
 
-switch (Number(day)) {
-    case 1: 
-    case 8: 
-    case 15: 
-    case 22: 
-    case 29:
-        alert("Суббота");
-        break;
+// for (num = 1; num <= 10; num++) {
+//     alert(num)
+// }
 
-    case 2: 
-    case 9: 
-    case 16: 
-    case 23: 
-    case 30:
-        alert("Воскресенье");
-        break;
 
-    case 3: 
-    case 10: 
-    case 17: 
-    case 24: 
-    case 31:
-        alert("Понедельник");
-        break;
+// let day = prompt("Какое сегодня число?")
 
-    case 4: 
-    case 11: 
-    case 18: 
-    case 25:
-        alert("Вторник");
-        break;
+// switch (Number(day)) {
+//     case 1: 
+//     case 8: 
+//     case 15: 
+//     case 22: 
+//     case 29:
+//         alert("Суббота");
+//         break;
 
-    case 5: 
-    case 12: 
-    case 19: 
-    case 26:
-        alert("Среда");
-        break;
+//     case 2: 
+//     case 9: 
+//     case 16: 
+//     case 23: 
+//     case 30:
+//         alert("Воскресенье");
+//         break;
 
-    case 6:
-    case 13: 
-    case 20: 
-    case 27:
-        alert("Четверг");
-        break;
+//     case 3: 
+//     case 10: 
+//     case 17: 
+//     case 24: 
+//     case 31:
+//         alert("Понедельник");
+//         break;
 
-    case 7: 
-    case 14: 
-    case 21: 
-    case 28:
-        alert("Пятница");
-        break;
-}
+//     case 4: 
+//     case 11: 
+//     case 18: 
+//     case 25:
+//         alert("Вторник");
+//         break;
+
+//     case 5: 
+//     case 12: 
+//     case 19: 
+//     case 26:
+//         alert("Среда");
+//         break;
+
+//     case 6:
+//     case 13: 
+//     case 20: 
+//     case 27:
+//         alert("Четверг");
+//         break;
+
+//     case 7: 
+//     case 14: 
+//     case 21: 
+//     case 28:
+//         alert("Пятница");
+//         break;
+// }
    
     
-let number = prompt("Какой сегодня число?")
+// let number = prompt("Какой сегодня число?")
 
-if(number >= 1 && number <= 2) {
-    alert(`${number} относится к первой неделе месяца`)
-}else if(number >= 3 && number <= 9) {
-    alert(`${number} относится ко второй неделе месяца`)
-}else if(number >= 10 && number <= 16) {
-    alert(`${number} относится к третий неделе месяца`)
-}else if(number >= 17 && number <= 23) {
-    alert(`${number} относится к четвертой неделе месяца`)
-}else if(number >= 24 && number <= 30) {
-    alert(`${number} относится к пятой неделе месяца`)
-}else if(number == 31) {
-    alert(`${number} относится к шестой неделе месяца`)
-}else {
-    alert('В месяце максиум 31 день')
+// if(number >= 1 && number <= 2) {
+//     alert(`${number} относится к первой неделе месяца`)
+// }else if(number >= 3 && number <= 9) {
+//     alert(`${number} относится ко второй неделе месяца`)
+// }else if(number >= 10 && number <= 16) {
+//     alert(`${number} относится к третий неделе месяца`)
+// }else if(number >= 17 && number <= 23) {
+//     alert(`${number} относится к четвертой неделе месяца`)
+// }else if(number >= 24 && number <= 30) {
+//     alert(`${number} относится к пятой неделе месяца`)
+// }else if(number == 31) {
+//     alert(`${number} относится к шестой неделе месяца`)
+// }else {
+//     alert('В месяце максиум 31 день')
+// }
+
+
+
+function multiply (a, b) {
+     return  a * b
 }
 
+console.log(multiply(10, 275))
+
+multiply()
+
+
+
+let age = Number(prompt("Сколько тебе лет?"))
+
+function isAdult (age) {
+
+     if(age >= 18) {
+        return true
+     }
+     return false
+}
+
+console.log(isAdult(age))
+
+isAdult()
+
+
+
+let a = Number(prompt("Какое число возвести в квадрат?"))
+
+const getSquare = () => {
+    return a ** 2
+}
+
+console.log(getSquare(a))
+
+getSquare()
